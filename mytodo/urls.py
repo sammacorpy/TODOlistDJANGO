ae1af593c8b74b68466724e9f637d489267fe929
sammacorpy <sammacorpy@gmail.com>
2018-04-27 22:21:08 +0530

    djangoUpdate

--- mytodo/urls.py
+++ mytodo/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import url
+
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.homepage,name='homepage'),
+    path('',views.homepage,name='homepage'),
     
-    url(r'^add$',views.addtask,name="add"),
-    url(r'^delete$',views.deletetask,name="delete"),
-    url(r'^macomp$',views.completetask,name="macomp"),
-    url(r'^activate$',views.activatetask,name="activate"),
-    url(r'^suspend$',views.suspendtask,name="suspend")
+    path('add',views.addtask,name="add"),
+    path('delete',views.deletetask,name="delete"),
+    path('macomp',views.completetask,name="macomp"),
+    path('activate',views.activatetask,name="activate"),
+    path('suspend',views.suspendtask,name="suspend")
 ]
