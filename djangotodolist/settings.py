0d4c2496af7156033866936c31d42aa4effd473c
Sam <sammacorpy@gmail.com>
2019-07-15 14:58:00 +0530

    dev config

--- djangotodolist/settings.py
+++ djangotodolist/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'djangotodolist.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
