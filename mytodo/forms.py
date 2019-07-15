1ba68ee242141393bd26ba56dc694ac62da3048b
Sam <sammacorpy@gmail.com>
2019-07-15 18:47:20 +0530

    dev conf

--- mytodo/forms.py
+++ mytodo/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from datetime import datetime 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class todoform(forms.Form):
     title=forms.CharField(max_length=40,widget=forms.TextInput(attrs={'placeholder':"Enter task title"}))
     desc=forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder':"Enter task description"}))
 
-    lastdate=forms.DateField()
+    lastdate=forms.DateField(widget=DateInput)
